5b856ab8bbf4adb18096a961922145353c658a7a
Budavati Manaswini <budavati.manaswini@gmail.com>
2016-07-12 16:18:11 +0530

    v3

--- rss_blog2wiki.py
+++ rss_blog2wiki.py
@@ -21,27 +21,6 @@ for link in li:
     links.append(link.get('href'))
     cate.append(link.text)
 for each in range(len(links)):
-	# links1=[]
-	# page1 = urllib.request.urlopen(url1).read()
-	# soup1 = BeautifulSoup(page1)
-	# li1 = soup1.findAll('h2', {'class': 'entry-title'})
-	# for i in li1:
-	# 	li2 = i.findAll('a', rel="bookmark")
-	# 	for x in li2:
-	# 		links1.append(x.get('href'))
-	# links2=list(set(links1))
-	# for y in links2:
-	# 	page2 = urllib.request.urlopen(y)
-	# 	responsesoup = bs4.BeautifulSoup(page2.read())
-	# 	content = ''
-	# 	title = ''
-	# 	content = responsesoup.find('div' , {'class': 'entry-content'})
-	# 	title = responsesoup.find('h1' , {'class': 'entry-title'})
-	# 	r = requests.post("http://wiki.infoassistants.net/wiki/api/add/"+title.text.replace("?", "") +"/", data={'content': content.encode('utf-8') , 'cate': cate1 })
-	# 	print(r.status_code, r.reason)
-	# 	if(r.status_code, r.reason == "404 Not Found"):
-	# 		error_catch.append(y)
-	# print (links[each]+'rss')
 	reddit_file = urllib.request.urlopen(links[each]+'rss')
 	reddit_data = reddit_file.read()
 	reddit_file.close()
